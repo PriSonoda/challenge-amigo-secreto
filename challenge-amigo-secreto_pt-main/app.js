19ed2604511bb99aa35d47657677b81bba0dbbc8
Priscila Sonoda <sonoda.priscila@gmail.com>
2025-09-05 15:37:11 -0300

    Update app.js

--- challenge-amigo-secreto_pt-main/app.js
+++ challenge-amigo-secreto_pt-main/app.js
@@ -37,4 +37,4 @@ function sortearAmigo() {
   document.getElementById('resultado').innerHTML = '<li>' + nomeSorteado + '</li>';
 
   alert('Amigo sorteado: ' + nomeSorteado);
-}//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+}
